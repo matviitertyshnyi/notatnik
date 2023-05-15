758433c816151496da6a52d6e1952acbdf370628
Gdempc <grzegorz.dempc@gmail.com>
2023-05-15 12:09:03 +0200

    woRKING STRUCTURE

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import { useStyles } from './styles';
+
+const styles = useStyles(); 
 
 const YourApp = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
+    <View style={styles.container}>
       <Text>Try editing me! 🎉</Text>
     </View>
   );
--- styles.ts
+++ styles.ts
@@ -4,7 +4,9 @@ import { StyleSheet } from 'react-native';
 export const useStyles = () => {
     
     const styles = StyleSheet.create({
-       
+       container:{
+        backgroundColor:"#33ffaa"
+       },
     });
     return styles;
 }
