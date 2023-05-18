f0547c42b1c4bca5a8d5a58eee52315461afec46
Gdempc <grzegorz.dempc@gmail.com>
2023-05-18 11:17:52 +0200

    scrollview and add button

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View,  StyleSheet, TouchableOpacity} from 'react-native';
+import {Text, View, SafeAreaView, StatusBar,StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import { useStyles } from './styles';
 import Note from './newNote';
 import { AddNote } from './assets';
@@ -9,15 +9,22 @@ const YourApp = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>My notes:</Text>
-        <Note/>
-        
-        <AddNote width={100} height={100}/>
-        <Note/>
-        <Note/>
-        <Note/>
-        <Note/>
-        <Note/>
-
+          <TouchableOpacity style={styles.addnote}>
+            <AddNote width={100} height={100}/>
+          </TouchableOpacity>
+          <SafeAreaView>
+            <ScrollView style={styles.scrollView}>
+              <Note/>
+              <Note/>
+              <Note/>
+              <Note/>
+              <Note/>
+              <Note/>
+              <Note/>
+              <Note/>
+              <Note/>
+            </ScrollView>
+          </SafeAreaView>
     </View>
     );
 };
--- styles.ts
+++ styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, StatusBar } from 'react-native';
 
 export const useStyles = () => {
     
@@ -17,7 +17,7 @@ export const useStyles = () => {
        },
        note: {
        width: "95%",
-       height: "15%",
+       height: 100,
        marginLeft: "auto",
        marginRight: "auto",
        backgroundColor:"#FFF",
@@ -32,7 +32,24 @@ export const useStyles = () => {
         fontSize: 25,
         fontFamily: 'lucida grande, tahoma, verdana, arial, sans-serif',
         color: "black",
-      }
+      },
+      addnote:{
+        width: "25%",
+        aspectRatio: 1/1,
+        position : "absolute",
+        bottom:20,
+        right:20,
+        backgroundColor: "#6f6f6f",
+        zIndex:2,
+        borderRadius:100,
+      },
+      scrollView: {
+        width:"100%",
+      },
+      SAV: {
+        flex: 1,
+        paddingTop: StatusBar.currentHeight,
+      },
       
    });
     return styles;
